d4c4be19477b650fca7ccfd6a442ee5dca35e752
Chris Cardle <cdcardle@gmail.com>
2017-10-26 18:44:28 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,6 +12,6 @@ end
 
 def assign_rooms(array)
   array.each do |name|
-    puts "Hello, #{name}! You'll be assigned to room #{array.index(name) + 1}"
+    return "Hello, #{name}! You'll be assigned to room #{array.index(name) + 1}"
   end
 end
