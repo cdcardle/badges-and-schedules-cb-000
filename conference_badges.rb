3742048f4a9402eefd076085f2422bdb7ce9eb28
Chris Cardle <cdcardle@gmail.com>
2017-10-26 16:46:50 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,9 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(array)
+  array2 = []
   array.each do |name|
-    badge_maker(name)
+    array2.push(badge_maker(name))
   end
+  return array2
 end
